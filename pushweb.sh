3529c6e91e65e0c187e6cd16877bb9d69e7d4150
Peter Williams <peter@newton.cx>
2011-07-25 16:00:35 -0700

    Publish manuals as PDFs, not gzipped PostScript.

--- pushweb.sh
+++ pushweb.sh
@@ -9,4 +9,7 @@ set -e -x
 
 cd web
 cp -p $files $dest
-cp -p /cosmic1/pkwill/miriad/$BADOS/share/doc/miriad/* $dest
+
+for f in /cosmic1/pkwill/miriad/$BADOS/share/doc/miriad/*.ps.gz ; do
+    zcat $f |ps2pdf - $dest$(basename $f |sed -e 's/ps.gz/pdf/')
+done
