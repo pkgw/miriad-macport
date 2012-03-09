d638d395237f1348246eed52926d796d2d4dff39
Peter Williams <peter@newton.cx>
2012-03-09 14:56:31 -0800

    pushweb.sh: update docs path for new install location

--- pushweb.sh
+++ pushweb.sh
@@ -10,6 +10,6 @@ set -e -x
 cd web
 cp -p $files $dest
 
-for f in /cosmic1/pkwill/miriad/$BADOS/share/doc/miriad/*.ps.gz ; do
+for f in /linux-apps4/miriad/share/doc/miriad/*.ps.gz ; do
     zcat $f |ps2pdf - $dest$(basename $f |sed -e 's/ps.gz/pdf/')
 done
